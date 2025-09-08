b932b651075b8f999c2029fd618b832ccbd3b07d
Pavan009 <kumarsangeetham9316@gmail.com>
2025-09-08 17:25:44 +0530

    19: meetings filters

--- src/modules/meetings/server/procedures.ts
+++ src/modules/meetings/server/procedures.ts
@@ -6,6 +6,7 @@ import { and, count, desc, eq, getTableColumns, ilike, sql, } from "drizzle-orm"
 import { DEFAULT_PAGE, DEFAULT_PAGE_SIZE, MAX_PAGE_SIZE, MIN_PAGE_SIZE } from "@/constants";
 import { TRPCError } from "@trpc/server";
 import { meetingsInsertSchema, meetingsUpdateSchema } from "../schema";
+import { MeetingStatus } from "../types";
 
 export const meetingsRouter = createTRPCRouter({
   update: protectedProcedure
@@ -76,13 +77,23 @@ export const meetingsRouter = createTRPCRouter({
       .min(MIN_PAGE_SIZE)
       .max(MAX_PAGE_SIZE)
       .default(DEFAULT_PAGE_SIZE),
-      search: z.string().nullish()
+      search: z.string().nullish(),
+      agentId: z.string().nullish(), 
+      status: z
+        .enum([
+          MeetingStatus.Upcoming,
+          MeetingStatus.Active,
+          MeetingStatus.Completed,
+          MeetingStatus.Processing,
+          MeetingStatus.Cancelled,
+        ])
+        .nullish(),
     })
   )
     .query(async ({ ctx, input}) => {
         console.log("CTX USER:", ctx.auth.user);
         console.log("INPUT:", input);
-        const { page, pageSize, search } = input;
+        const {search, page, pageSize, status, agentId } = input;
         const data = await db
         .select({
           ...getTableColumns(meetings),
@@ -94,7 +105,9 @@ export const meetingsRouter = createTRPCRouter({
         .where(
           and(
             eq(meetings.userId, ctx.auth.user.id),
-            search ? ilike(meetings.name, `%${search}%`) : undefined
+            search ? ilike(meetings.name, `%${search}%`) : undefined,
+            status ? eq(meetings.status, status) : undefined,
+            agentId ? eq(meetings.agentId, agentId) : undefined,
           )
         )
         .orderBy(desc(meetings.createdAt), desc(meetings.id))
@@ -108,7 +121,9 @@ export const meetingsRouter = createTRPCRouter({
         .where(
           and(
             eq(meetings.userId, ctx.auth.user.id),
-            search ? ilike(meetings.name, `%${search}%`) : undefined
+            search ? ilike(meetings.name, `%${search}%`) : undefined,
+            status ? eq(meetings.status, status) : undefined,
+            agentId ? eq(meetings.agentId, agentId) : undefined,
           )
         );
 
--- src/modules/meetings/ui/components/meetings-list-header.tsx
+++ src/modules/meetings/ui/components/meetings-list-header.tsx
@@ -1,13 +1,31 @@
 "use client"
 
 import { Button } from "@/components/ui/button";
-import { PlusIcon } from "lucide-react";
+import { PlusIcon, XCircleIcon } from "lucide-react";
 import { useState } from "react";
 import { NewMeetingDialog } from "./new-meeting-dialog";
+import { MeetingsSearchFilters } from "./meetings-search-filter";
+import { StatusFilter } from "./status-filters";
+import { AgentIdFilter } from "./agent-id-filter";
+import { useMeetingsFilters } from "../../hooks/use-meetings-filters";
+import { ScrollArea, ScrollBar } from "@/components/ui/scroll-area";
+import { DEFAULT_PAGE } from "@/constants";
 
 
 const MeetingsListHeader = () => {
-    const [isDialogOpen, setIsDialogOpen] = useState(false)
+    const [filters, setFilters] = useMeetingsFilters();
+    const [isDialogOpen, setIsDialogOpen] = useState(false);
+
+    const isAnyFilterModified = !!filters.status || !!filters.search || !!filters.agentId;
+
+    const onClearFilters = () => {
+        setFilters({
+            status: null,
+            search: "",
+            agentId: "",
+            page: DEFAULT_PAGE,
+        })
+    }
     return ( 
     <>
     <NewMeetingDialog open={isDialogOpen} onOPenChange={setIsDialogOpen}/>
@@ -19,9 +37,20 @@ const MeetingsListHeader = () => {
                 New Meeting
             </Button>
         </div>
-        <div className="flex items-center gap-x-2 p-1">
-            TODO: Filters
-        </div>
+        <ScrollArea>
+            <div className="flex items-center gap-x-2 p-1">
+                <MeetingsSearchFilters/>
+                <StatusFilter/>
+                <AgentIdFilter/>
+                {isAnyFilterModified && (
+                    <Button variant={"outline"} onClick={onClearFilters}>
+                        <XCircleIcon className="size-4" />
+                        Clear
+                    </Button>
+                )}
+            </div>
+            <ScrollBar orientation="horizontal" />
+        </ScrollArea>
     </div>
     </>
      );
