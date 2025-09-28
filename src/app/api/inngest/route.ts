74897ace6286e845a3b45e6fc0438c92e95e8727
Pavan009 <kumarsangeetham9316@gmail.com>
2025-09-28 22:12:14 +0530

    fixed error #4

--- src/app/api/inngest/route.ts
+++ src/app/api/inngest/route.ts
@@ -1,15 +1,28 @@
+// import { serve } from "inngest/next";
+// import { inngest } from "@/inngest/client";
+// import { meetingsProcessing } from "@/inngest/functions";
+
+// export const { GET, POST, PUT } = serve({
+//   client: inngest,
+//   // baseUrl: "http://localhost:3000/api/inngest",
+//   functions: [meetingsProcessing],
+// });
+
+// // export const config = {
+// //   api: {
+// //     bodyParser: false, 
+// //   },
+// // };
+
 import { serve } from "inngest/next";
 import { inngest } from "@/inngest/client";
 import { meetingsProcessing } from "@/inngest/functions";
 
-export const { GET, POST, PUT } = serve({
+const handler = serve({
   client: inngest,
-  // baseUrl: "http://localhost:3000/api/inngest",
   functions: [meetingsProcessing],
 });
 
-// export const config = {
-//   api: {
-//     bodyParser: false, 
-//   },
-// };
+export const GET = handler.GET;
+export const POST = handler.POST;
+export const PUT = handler.PUT;
