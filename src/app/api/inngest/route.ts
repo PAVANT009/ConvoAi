7b1fb88ad2954c823d2d32c2535f004e5d5bf240
Pavan009 <kumarsangeetham9316@gmail.com>
2025-09-30 11:33:51 +0530

    added PUT in inngest

--- src/app/api/inngest/route.ts
+++ src/app/api/inngest/route.ts
@@ -19,8 +19,7 @@ import { serve } from "inngest/next";
 import { inngest } from "@/inngest/client";
 import { meetingsProcessing } from "@/inngest/functions";
 
-export const { GET, POST } = serve({
+export const { GET, POST, PUT } = serve({
   client: inngest,
   functions: [meetingsProcessing],
-  baseUrl: process.env.INNGEST_BASE_URL,
 });
